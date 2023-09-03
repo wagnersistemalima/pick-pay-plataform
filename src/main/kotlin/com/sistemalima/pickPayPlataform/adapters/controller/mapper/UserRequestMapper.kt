9b9305bd6a995fa15dec0e5fcb95ccadfc664640
wagnersistemalima <wagner.sistemalima@gmail.com>
2023-09-02 23:03:55 -0300

    setting business rule in classes and their responsibilities

--- src/main/kotlin/com/sistemalima/pickPayPlataform/adapters/controller/mapper/UserRequestMapper.kt
+++ src/main/kotlin/com/sistemalima/pickPayPlataform/adapters/controller/mapper/UserRequestMapper.kt
@@ -1,20 +1,10 @@
 package com.sistemalima.pickPayPlataform.adapters.controller.mapper
 
 import com.sistemalima.pickPayPlataform.adapters.controller.entity.UserRequest
-import com.sistemalima.pickPayPlataform.adapters.controller.mapper.UserRequestMapper.toAccounting
-import com.sistemalima.pickPayPlataform.adapters.controller.mapper.UserRequestMapper.toDomain
-import com.sistemalima.pickPayPlataform.adapters.controller.mapper.UserRequestMapper.toModel
 import com.sistemalima.pickPayPlataform.domain.User
-import com.sistemalima.pickPayPlataform.domain.exceptions.BusinessException
 
 object UserRequestMapper {
-
-    private const val CNPJ = 14
-    private const val CPF = 11
-
     fun UserRequest.toDomain(): User {
-
-        validatedUserType()
 
         return User(
             name = this.name,
@@ -37,16 +27,5 @@ object UserRequestMapper {
         return User.Accounting(
             balance = this.accounting.balance
         )
-    }
-
-    private fun UserRequest.validatedUserType(): Boolean {
-
-        return if (this.document.length == CNPJ && this.userType == UserRequest.UserRequestTypeEnum.MERCHANT_PERSON) {
-            true
-        } else if (this.document.length == CPF && this.userType == UserRequest.UserRequestTypeEnum.COMMON_PERSON) {
-            true
-        } else {
-            throw BusinessException("common person or merchant type validation error")
-        }
     }
 }
